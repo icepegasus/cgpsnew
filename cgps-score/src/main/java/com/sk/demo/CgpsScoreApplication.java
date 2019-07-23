5f25a853ee1c47fd6e538badea1788352c91f232
swj9189 <swj9189@naver.com>
2019-07-23 16:30:07 +0900

    init

---
+++ cgps-score/src/main/java/com/sk/demo/CgpsScoreApplication.java
@@ -0,0 +1,13 @@
+package com.sk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgpsScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgpsScoreApplication.class, args);
+	}
+
+}
---
+++ cgps-user/src/main/java/com/sk/demo/CgpsUserApplication.java
@@ -0,0 +1,13 @@
+package com.sk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgpsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgpsUserApplication.class, args);
+	}
+
+}
